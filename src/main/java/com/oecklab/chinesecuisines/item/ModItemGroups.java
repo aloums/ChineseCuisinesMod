4180cffc8c9fe1892f2479dd0ce77942e298820d
aloums <335759051@qq.com>
2023-11-28 18:08:43 +0800

    Initial commit

--- src/main/java/com/oecklab/chinesecuisines/item/ModItemGroups.java
+++ src/main/java/com/oecklab/chinesecuisines/item/ModItemGroups.java
@@ -18,15 +18,6 @@ public class ModItemGroups {
                         entries.add(ModItems.FRIED_RING);
                         entries.add(ModItems.SALTED_VEGETABLE);
                     }).build());
-//    public static final ItemGroup CHINESE_CUISINES_GROUP = Registry.register(Registries.ITEM_GROUP,
-//            new Identifier(ChineseCuisines.MOD_ID, "Chinese Cuisine"),
-//            FabricItemGroup.builder().displayName(Text.translatable("itemgroup.chinese_cuisines"))
-//                    .icon(() -> new ItemStack(ModItems.BEAN_JUICE)).entries((displayContext, entries) -> {
-//                        entries.add(ModItems.BEAN_JUICE);
-//                        entries.add(ModItems.FRIED_RING);
-//                        entries.add(ModItems.SALTED_VEGETABLE);
-//                    }).build());
-
     public static void registerItemGroups() {
         ChineseCuisines.LOGGER.info("Registering Item Groups for " +ChineseCuisines.MOD_ID);
     }
